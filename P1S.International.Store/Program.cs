86d04f5107af6792d37f1e8c6979b57ce0ac56ec
David Kalsbeek <David Kalsbeek@DESKTOP-N81REFS>
2017-07-01 22:46:30 -0500

    Add project files.

---
+++ P1S.International.Store/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1S.International.Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
